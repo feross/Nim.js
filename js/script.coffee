6480f20873410cf70f7017774ded0bfd232bcc17
Feross Aboukhadijeh <feross@gmail.com>
2012-03-03 22:28:06 -0800

    Presentation

--- js/script.coffee
+++ js/script.coffee
@@ -1,43 +1,100 @@
 numStones = 15
 playerTurn = 1 # 0: human player, 1: computer player
+numSelectedStones = 0
+computerWins = 0
+humanWins = 0
 
 # need to always leave human player with 4, 8, or 12 stones
 
-playTurn = (player, numStonesToRemove) ->
-  numStones -= numStonesToRemove
-  
-  playerTurn = (playerTurn == 1) ? 0 : 1
+delay = (ms, func) -> window.setTimeout func, ms
 
-  console.log player + " removed " + numStonesToRemove + " stones."
-  console.log numStones + " stones left."
-  console.log "-------"
+isValidMove = ->
+  return (numStones - numSelectedStones >= 0 && 1 <= numSelectedStones <= 3)
 
-  if (numStones == 0)
-    console.log player + " wins!"
-    handleComputerWin()
-
-
-isValidMove = (numStonesToRemove) ->
-  return (numStones - numStonesToRemove >= 0)
-
-window.handleHumanMove = (numStonesToRemove) ->
-  if (isValidMove(numStonesToRemove))
-    playTurn 0, numStonesToRemove
+handleHumanMove = ->
+  if (isValidMove())
+    playTurn 0
   else
     alert "Invalid move!"
 
-  doComputerMove()
+removeSelectedStones = ->
+  $('.stone.selected').removeClass('selected').hide()
+
+  numSelectedStones = 0
+  if playerTurn == 1
+    turnText = "Your Turn"
+    $('html').addClass 'player'
+  else
+    turnText = "Computer's Turn"
+    $('html').removeClass 'player'
+
+
+  $('.status').html "#{numStones} stones left. " + turnText + "."
+
+handleComputerWin = ->
+  computerWins += 1
+  $('.stone').show()
+  numStones = 15
+  playerTurn = 1
+  numSelectedStones = 0
+
+  $('.computerScore span').text computerWins
+  $('.status').html "#{numStones} stones left."
+  delay 1000, ->
+    doComputerMove();
+
+playTurn = (player) ->
+  numStones -= numSelectedStones
+  
+  console.log player + " removed " + numSelectedStones + " stones."
+  console.log numStones + " stones left."
+  console.log "-------"
+
+  removeSelectedStones()
+
+  if (player == 0)
+    doComputerMove()
+
+  playerTurn = (playerTurn == 1) ? 0 : 1
 
 doComputerMove = ->
-  numStonesToRemove =
+  numSelectedStones =
     if (numStones > 4)
       numStones % 4
     else
       numStones
   
-  playTurn 1, numStonesToRemove
+  stones = $('.stone:not(:hidden)')[0...numSelectedStones]
 
-doComputerMove()
+  msDelay = 0
+  stones.each (i, e) ->
+    msDelay += 500
+    delay msDelay, ->
+      $(e).addClass 'selected'
+  
+  msDelay += 500
+  delay msDelay, ->
+    playTurn 1
 
-handleComputerWin = ->
-  # do something
+    if (numStones == 0)
+      console.log playerTurn + " wins!"
+      handleComputerWin()
+
+$ ->
+  $(document).on 'click', '.go', (e) ->
+    if (isValidMove)
+      handleHumanMove()
+
+  $(document).on 'click', '.stone', (e) ->
+    elem = $(e.target)
+
+    if (elem.hasClass 'selected')
+      numSelectedStones -= 1
+      elem.removeClass 'selected'
+    else
+      if (numSelectedStones < 3)
+        numSelectedStones += 1
+        elem.addClass 'selected'
+
+  delay 1000, ->
+    doComputerMove()
--- js/script.js
+++ js/script.js
@@ -1,46 +1,113 @@
 (function() {
-  var doComputerMove, handleComputerWin, isValidMove, numStones, playTurn, playerTurn;
+  var computerWins, delay, doComputerMove, handleComputerWin, handleHumanMove, humanWins, isValidMove, numSelectedStones, numStones, playTurn, playerTurn, removeSelectedStones;
 
   numStones = 15;
 
   playerTurn = 1;
 
-  playTurn = function(player, numStonesToRemove) {
+  numSelectedStones = 0;
+
+  computerWins = 0;
+
+  humanWins = 0;
+
+  delay = function(ms, func) {
+    return window.setTimeout(func, ms);
+  };
+
+  isValidMove = function() {
+    return numStones - numSelectedStones >= 0 && (1 <= numSelectedStones && numSelectedStones <= 3);
+  };
+
+  handleHumanMove = function() {
+    if (isValidMove()) {
+      return playTurn(0);
+    } else {
+      return alert("Invalid move!");
+    }
+  };
+
+  removeSelectedStones = function() {
+    var turnText;
+    $('.stone.selected').removeClass('selected').hide();
+    numSelectedStones = 0;
+    if (playerTurn === 1) {
+      turnText = "Your Turn";
+      $('html').addClass('player');
+    } else {
+      turnText = "Computer's Turn";
+      $('html').removeClass('player');
+    }
+    return $('.status').html(("" + numStones + " stones left. ") + turnText + ".");
+  };
+
+  handleComputerWin = function() {
+    computerWins += 1;
+    $('.stone').show();
+    numStones = 15;
+    playerTurn = 1;
+    numSelectedStones = 0;
+    $('.computerScore span').text(computerWins);
+    $('.status').html("" + numStones + " stones left.");
+    return delay(1000, function() {
+      return doComputerMove();
+    });
+  };
+
+  playTurn = function(player) {
     var _ref;
-    numStones -= numStonesToRemove;
-    playerTurn = (_ref = playerTurn === 1) != null ? _ref : {
-      0: 1
-    };
-    console.log(player + " removed " + numStonesToRemove + " stones.");
+    numStones -= numSelectedStones;
+    console.log(player + " removed " + numSelectedStones + " stones.");
     console.log(numStones + " stones left.");
     console.log("-------");
-    if (numStones === 0) {
-      console.log(player + " wins!");
-      return handleComputerWin();
-    }
-  };
-
-  isValidMove = function(numStonesToRemove) {
-    return numStones - numStonesToRemove >= 0;
-  };
-
-  window.handleHumanMove = function(numStonesToRemove) {
-    if (isValidMove(numStonesToRemove)) {
-      playTurn(0, numStonesToRemove);
-    } else {
-      alert("Invalid move!");
-    }
-    return doComputerMove();
+    removeSelectedStones();
+    if (player === 0) doComputerMove();
+    return playerTurn = (_ref = playerTurn === 1) != null ? _ref : {
+      0: 1
+    };
   };
 
   doComputerMove = function() {
-    var numStonesToRemove;
-    numStonesToRemove = numStones > 4 ? numStones % 4 : numStones;
-    return playTurn(1, numStonesToRemove);
+    var msDelay, stones;
+    numSelectedStones = numStones > 4 ? numStones % 4 : numStones;
+    stones = $('.stone:not(:hidden)').slice(0, numSelectedStones);
+    msDelay = 0;
+    stones.each(function(i, e) {
+      msDelay += 500;
+      return delay(msDelay, function() {
+        return $(e).addClass('selected');
+      });
+    });
+    msDelay += 500;
+    return delay(msDelay, function() {
+      playTurn(1);
+      if (numStones === 0) {
+        console.log(playerTurn + " wins!");
+        return handleComputerWin();
+      }
+    });
   };
 
-  doComputerMove();
-
-  handleComputerWin = function() {};
+  $(function() {
+    $(document).on('click', '.go', function(e) {
+      if (isValidMove) return handleHumanMove();
+    });
+    $(document).on('click', '.stone', function(e) {
+      var elem;
+      elem = $(e.target);
+      if (elem.hasClass('selected')) {
+        numSelectedStones -= 1;
+        return elem.removeClass('selected');
+      } else {
+        if (numSelectedStones < 3) {
+          numSelectedStones += 1;
+          return elem.addClass('selected');
+        }
+      }
+    });
+    return delay(1000, function() {
+      return doComputerMove();
+    });
+  });
 
 }).call(this);
